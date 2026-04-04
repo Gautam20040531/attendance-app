078b9af0dc6f22a72185f22c06f05499de6cc0df
Gautam Pal <gp2285172@gmail.com>
2026-04-05 01:27:19 +0530

    Updated dashboard and API

---
+++ pages/api/attendance.js
@@ -0,0 +1,28 @@
+// pages/api/attendance.js
+const studentsDB = {
+  "B417AB04": { name: "Gautam Pal", status: "Present" },
+  "ADABD505": { name: "Student 2 (Keychain)", status: "Present" }
+};
+
+let latestAttendance = null;
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { uid } = req.body;
+    
+    if (studentsDB[uid]) {
+      latestAttendance = {
+        name: studentsDB[uid].name,
+        time: new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' }),
+        uid: uid
+      };
+      return res.status(200).json({ success: true, message: "Attendance Logged" });
+    } else {
+      return res.status(404).json({ success: false, message: "Unknown Card" });
+    }
+  } 
+  
+  else if (req.method === 'GET') {
+    return res.status(200).json({ latestAttendance });
+  }
+}
